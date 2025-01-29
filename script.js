38bbaecd560e0126c5984679bceec1a9c480868a
post.service <consultant.postservice@gmail.com>
2025-01-29 17:38:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const userInput = document.getElementById("userInput");
 const parcelMessage = document.getElementById("parcelMessage")
 const errorNote = document.getElementById("errorNote")
+const errorMsg = document.getElementById("errorMsg");
 const submitButton = document.getElementById("submitButton")
 const trackNo = "4861054262";
 
 submitButton.addEventListener("click", function () {
     const input = userInput.value.trim(); // Get the input value and trim white space
 
-    if (input === trackNo) {
+    if (input == "") {
+        errorNote.style.display = "block";
+        errorMsg.innerHTML = "please enter tracking number!";
+    }
+    else if (input === trackNo) {
         parcelMessage.classList.add("track-poistion-see");
         errorNote.style.display = "none"
     }
     else {
         parcelMessage.classList.remove("track-poistion-see")
         errorNote.style.display = "block"
+        errorMsg.innerHTML = "Incorrect tracking number! Please try again or contact Agent.";
     }
 });
 
